ffbb6e14a88dd29c3f2a26b58a4fe58b498eb7e5
Md. Hasnain Ali <mdhasnainali.01@gmail.com>
2020-06-22 21:17:35 +0600

    add cpp template

---
+++ My Templates/C++ Template.cpp
@@ -0,0 +1,22 @@
+//MD. HASNAIN ALI (UNSOLVED)
+#include<bits/stdc++.h>
+using namespace std;
+
+#pragma GCC optimize ("Ofast")
+#pragma GCC optimize("O3")
+#define ll long long int
+#define end "\n"
+#define distance(a,b) (sq(a.x-b.x) + sq(a.y-b.y))
+#define FastIO ios_base::sync_with_stdio(0);cin.tie(nullptr);
+#define eps 10e-9
+#define MX 1000005
+
+
+int main()
+{
+    //freopen("test.txt","r",stdin);
+
+
+
+
+}
