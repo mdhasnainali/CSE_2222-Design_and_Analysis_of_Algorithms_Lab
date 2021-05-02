3eba3497d8f057b5b87c1803a1642e8fed589b2c
mdhasnainali <mdhasnainali.01@gmail.com>
2021-05-03 02:21:45 +0600

    Update Template, add DFS

--- My Templates/C++ Template.cpp
+++ My Templates/C++ Template.cpp
@@ -1,40 +1,31 @@
 // "Happiness is just a choice and life is beautiful"
 
-
 #include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp> 
+#include<ext/pb_ds/detail/standard_policies.hpp>
+using namespace __gnu_pbds;
 using namespace std;
-using namespace std::chrono;
- 
-#pragma GCC                     optimize ("Ofast")
-#pragma GCC                     optimize("O3")
-#define db                      double
+
 #define ll                      long long
 #define ull                     unsigned long long
-#define lo(i,a,n,x)             for(i=a;i<=n;i=i+x)
-#define loi(i,a,n,x)            for(i=a;i>=n;i=i-x) 
-#define sc                      scanf
-#define pr                      printf
-#define ms(a,b)                 memset(a, b, sizeof(a))
+#define MEMSET(a,b)                 memset(a, b, sizeof(a))
 #define pb(a)                   push_back(a)
 #define pop()                   pop_back()
 #define mp                      make_pair
 #define VI                      vector <int>
 #define PII                     pair <int,int>
-#define PLL                     pair <long long,long long>
 #define ff                      first
 #define ss                      second
 #define sqr(x)                  (x)*(x)
 #define SZ(a)                   (int)a.size()
 #define FAST_CIN                ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-#define stlloop(v)              for(__typeof(v.begin()) it=v.begin();it!=v.end();it++)
+#define stlloop(v)              for(auto it:v)
 #define UNIQUE(v)               (v).erase(unique((v).begin(),(v).end()),(v).end())
 #define POPCOUNT(m)             __builtin_popcountll(m)
 #define RIGHTMOST(m)            __builtin_ctzll(m)
 #define LEFTMOST(x)             (63-__builtin_clzll((x)))
 #define ODD(x)                  (((x)&1)==0?(0):(1))
-#define TEST_CASE(t)            for(int love=1;love<=t;love++)
-#define PRINT_CASE              printf("Case %d: ",love)
-#define LINE_PRINT_CASE         printf("Case %d:\n",z)
 #define all(a)                  a.begin(),a.end()
 #define SORT(x)                 sort(x.begin(), x.end())
 #define REV(x)                  reverse(x.begin(), x.end())
@@ -42,14 +33,11 @@ using namespace std::chrono;
 #define DBG(...)                cerr << __LINE__ <<':' << "(" << #__VA_ARGS__ << ")"<< " = ", dbg_out(__VA_ARGS__)
 #define GCD(a, b)               __gcd(a, b)
 #define LCM(a, b)               (a)*((b)/GCD(a,b))
-#define end0                    "\n"
-#define end1                    cout<<"\n";
 #define Pi                      acos(-1)
 #define mod                     1000000007
 #define intlim                  2147483648
 #define infinity                (1<<28)
 #define EPS                     10E-9            
-ll counts = 1;
 
 //----------------------------------------------------------------
 void dbg_out() { cerr << endl; }
@@ -59,12 +47,50 @@ void dbg_out(Head H, Tail... T) {
      dbg_out(T...);
 }
 //----------------------------------------------------------------
- 
-
-
-
-void solve()
+void stressTestr()
 {
+	while (true)
+	{
+		int n = rand() % 10 + 2;
+		cout << n << endl;
+		vector<int> numbers;
+		for (int i = 0; i < n ; i++)
+		{
+			numbers.push_back(rand() % 1000000);
+		}
+		for (int i = 0; i < n ; i++)
+		{
+			cout << numbers[i] << " ";
+		}
+		cout << endl;
+
+		long long result1; 
+		long long result2;
+
+		if (result1 != result2)
+		{
+			cout << "Wrong Answer" << result1 << " " << result2 << endl;
+			break;
+		}
+		else
+		{
+			cout << "OK!" << endl;
+		}
+	}
+}
+//----------------------------------------------------------------
+const int mx = 10005;
+vector <int> graph[mx];
+bool visited[mx];
+vector <int> knocked;
+//----------------------------------------------------------------
+
+
+
+
+void mainSolve()
+{
+    
     
 }
  
@@ -72,27 +98,25 @@ void solve()
 
 int main()
 {
-    #ifndef ONLINE_JUDGE 
-        freopen("input-2.txt","r",stdin);
-        //freopen("output-2.txt","w",stdout);
-        auto start=high_resolution_clock::now();
-    #endif
+    FAST_CIN;
+    #ifndef ONLINE_JUDGE
+        freopen("input-1.txt","r",stdin);
+        // freopen("output-1.txt","w",stdout);
+
+    #endif 
 //----------------------------------------------------------------    
-    
-    ll t;
+
+    ll t, result;
     cin>>t;
-    //t=1;
-    TEST_CASE(t)
-    {
-        //PRINT_CASE;
-        solve();
+    // t=1;
+
+    for(int i = 1; i<=t ; i++){
+
+        mainSolve();
+
+        // PRINT_CASE:
+        // cout<<"Case "<<i<<": "<<result<<endl;
     }
- 
-//----------------------------------------------------------------  
-    #ifndef ONLINE_JUDGE 
-        auto stop=high_resolution_clock::now();
-        auto duration=duration_cast<microseconds>(stop-start);
-        cerr<<"\n(Time:"<<duration.count()/1000000.0<<" Seconds)"<<endl;
-    #endif
+
     return 0;
 }
